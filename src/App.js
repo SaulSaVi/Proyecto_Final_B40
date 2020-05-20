c3200675812fc7b9c4e2479e29b21767e4e3cc77
natashaDianeey <ndianeey@gmail.com>
2020-05-20 12:19:37 -0500

    registro de usuario, email y password conectado con firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const createUser = (event) => {
+    event.preventDefault();
+    // FIREBASE NAT
+    // axios.post('https://proyectofinalb40.firebaseio.com/users.json', 
+    // FIREBASE GRECIA
+    axios.post('https://playlist-app-918cf.firebaseio.com/users.json', 
+
+    //// key:value,key:value,key:value,
+    {email, password}).then((response) => {
+      console.log(response.status)
+      alert('Tu usuario se ha registrado');
+      // clear();
+    }).catch(() => {
+      alert('Hubo un problema al crear tu usuario');
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-8 col-md-8 col-sm-8">
+            <form action="" onSubmit={createUser}>
+              <div className="form-group">
+                <label className="text-left flex" htmlFor="">User:</label>
+                <input type="text" className="form-control" name="email" placeholder="Name" required
+                  value={email} onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label className="text-left flex" htmlFor="">Password:</label>
+                <input type="password" className="form-control" name="password" placeholder="Password" required
+                  value={password} onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <button type="submit" className="btn btn-info">Save</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
