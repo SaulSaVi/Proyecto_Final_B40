1b6a5b9ccb9b6851086b5d67331ceb9c01c937c5
natashaDianeey <ndianeey@gmail.com>
2020-05-20 19:15:44 -0500

    funcionalidad de search, sin terminar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -24,8 +24,40 @@ function App() {
     })
   }
 
+
+  // search
+  // const URL = 'https://api.giphy.com/v1/gifs/search?api_key=ttAm40hnMkgbyQpH8pziLa1cC6Dm5l6a&q=';
+  const [search, setSearch] = useState('love'); // La palabra por defecto sera rugrats al cargar la pagina
+  const [gifs, setGifs] = useState([]);
+  const URL = `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=a3baf3f68415f9e146751276a90005f7&artist=kygo&track=${search}&format=json`;
+  useEffect(() => {
+    console.log('Desde use effect')
+    console.log(search)
+
+    axios.get(URL).then((response) => {
+      // En la siguiente liena de codigo se guarda 
+      setGifs(response.data) // primer data es de axios, segundo data es de respuesta de giphy
+    }).catch((error) => {
+      console.log(error)
+    })
+  },[])
+
+  const findGiphy = (event) => {
+    event.preventDefault()  //Evita que se refresque la pagina al momento de hacer submit en el form
+    axios.get(URL).then((response) => {
+      // En la siguiente liena de codigo se guarda 
+      console.log('response data: ',response.data)
+      console.log('response name: ',response.data.track.name)
+      setGifs(response.data.data) // primer data es de axios, segundo data es de respuesta de giphy
+    }).catch((error) => {
+      console.log(error)
+    }) 
+  }
+
+
   return (
     <div className="App">
+      {/* REGISTRO */}
       <div className="container">
         <div className="row">
           <div className="col-12 col-lg-8 col-md-8 col-sm-8">
@@ -45,6 +77,44 @@ function App() {
               <button type="submit" className="btn btn-info">Save</button>
             </form>
           </div>
+        </div>
+      </div>
+      {/* SEARCH */}
+      <div className="container">
+        SEARCH
+        <div className="container my-5">
+          <div className="row justify-content-center">
+            <div className="col-12 col-sm-8 col-lg-8">
+              <form onSubmit={findGiphy} className="form-inline justify-content-center">
+                <input className="form-control" 
+                  onChange={(event) => setSearch(event.target.value)} 
+                  placeholder={` ${search}`} 
+                  name="gif"
+                />
+                <button type="submit" className="btn btn-success">Buscar</button>
+              </form>
+            </div>
+          </div>
+
+          <div className="row mt-5">
+            { gifs.length > 0 ? gifs.map((gif) => { 
+              return(
+
+                <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+                  <div className="card bg-dark text-white">
+                      <h5 className="card-title">{gif}</h5>
+                  </div>
+                </div>
+              )
+            }) : (
+                <div className="col-12">
+                  <h3>No hay resultados </h3>
+                </div>
+              )
+            }
+          </div>
+
+          {/* <h5 className="card-title">{gifs}</h5> */}
         </div>
       </div>
     </div>
